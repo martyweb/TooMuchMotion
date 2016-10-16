75ce97dbafcd897e80b76eb618741f95e0a7ed8b
martyweb <marty@martyweb.com>
2016-10-16 15:28:31 -0500

    Added lights
    Added a green/red light to indicate motion

--- TooMuchMotion.ino
+++ TooMuchMotion.ino
@@ -1,9 +1,37 @@
+int pirPin = 5;
+int redPin = 5;
+int greenPin = 10;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(pirPin, INPUT);
+  //pinMode(redPin, OUTPUT);
+  //pinMode(greenPin, OUTPUT);
 
+
+//  digitalWrite(redPin, HIGH);
+//  delay(500);
+//  digitalWrite(redPin, LOW);
+//  digitalWrite(greenPin, HIGH);
+//  delay(500);
+//  digitalWrite(greenPin, LOW);
+  
+  Serial.begin(9600);
+  Serial.println("All setup and ready to go");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  Serial.println(digitalRead(pirPin));
+  if (digitalRead(pirPin) == HIGH)
+  {
+    //delay(200);
+    Serial.println("MOVEMENT");
+    //digitalWrite(redPin, HIGH);
+  }else{
+    Serial.println("Still");
+    //digitalWrite(redPin, LOW);
+  }
+  delay(500);
 }
